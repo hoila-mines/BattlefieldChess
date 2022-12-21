16e27b039e38b2d2ce82561a54143db50464a3a0
hoila-mines <112978576+hoila-mines@users.noreply.github.com>
2022-12-20 17:09:37 -0700

    Created pygame window

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
+import pygame
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pygame.display
 
+SIZE = WIDTH, HEIGHT = 720, 720
+WINDOW = pygame.display.set_mode(SIZE)
+pygame.display.set_caption("Battlefield Chess")
+WINDOW.fill((128, 128, 128))
+FPS_limit = 60
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    FPS_clock = pygame.time.Clock()
+    is_running = True
+    while is_running:
+        FPS_clock.tick(FPS_limit)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                is_running = False
+        pygame.display.update()
+    pygame.quit()
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    main();
