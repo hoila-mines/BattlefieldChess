57e387c262501b26c5d8e1f9579f3f63d6481ab9
hoila-mines <112978576+hoila-mines@users.noreply.github.com>
2023-01-17 00:22:55 -0700

    Displays captured pieces

--- main.py
+++ main.py
@@ -13,26 +13,27 @@ from piece_color import PieceColor
 from player import Player
 from queen import Queen
 from rook import Rook
-#setup game window
+
+# setup game window
 WINDOW_SIZE = WINDOW_WIDTH, WINDOW_HEIGHT = 1078, 820
 window = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption("Battlefield Chess")
 FPS_LIMIT = 60
-#calculate board start and square size
+# calculate board start and square size
 BOARD_WIDTH, BOARD_HEIGHT = 22, 12
-SQUARE_SIZE = min(WINDOW_WIDTH/BOARD_WIDTH, WINDOW_HEIGHT/BOARD_HEIGHT)
+SQUARE_SIZE = min(WINDOW_WIDTH / BOARD_WIDTH, WINDOW_HEIGHT / BOARD_HEIGHT)
 Y_OFFSET = (WINDOW_HEIGHT - SQUARE_SIZE * BOARD_HEIGHT) / 2
-#define colors
+# define colors
 WHITE_COLOR = (255, 255, 255)
 RED_COLOR = (255, 128, 128)
-HIGHLIGHT_COLOR = (51,153, 255)
+HIGHLIGHT_COLOR = (51, 153, 255)
 # initiate players and board
 white_player = Player()
 black_player = Player()
 player_turn = PieceColor.WHITE
 board = [[None for i in range(BOARD_WIDTH)] for j in range(BOARD_HEIGHT)]
 # highlighted_piece = None
-sprite_cache = {}
+image_cache = {}
 
 board_config = [
     ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR", "bN", "bB", "bQ", "bK",
@@ -55,12 +56,12 @@ board_config = [
 
 
 def read_config():
-    for i in range(len(board_config)): #read through each row
-        if len(board_config[i]) == 0: # if the array element is empty, there are no pieces to read
+    for i in range(len(board_config)):  # read through each row
+        if len(board_config[i]) == 0:  # if the array element is empty, there are no pieces to read
             continue
         else:
-            for j in range(len(board_config[i])): #read through each column
-                #get piece info in character representation
+            for j in range(len(board_config[i])):  # read through each column
+                # get piece info in character representation
                 piece_color_char = board_config[i][j][0]
                 piece_type_char = board_config[i][j][1]
                 piece_color = None
@@ -99,7 +100,7 @@ def alternate_white_red(color):  # swap the color from red to white, or vise ver
         return RED_COLOR
 
 
-def draw_board(): #draw the squares in alternating color
+def draw_board():  # draw the squares in alternating color
     color = WHITE_COLOR
     for i in range(BOARD_HEIGHT):
         color = alternate_white_red(color)
@@ -107,27 +108,51 @@ def draw_board(): #draw the squares in alternating color
             color = alternate_white_red(color)
             square = pygame.Rect(j * SQUARE_SIZE, i * SQUARE_SIZE + Y_OFFSET, SQUARE_SIZE, SQUARE_SIZE)
             piece = board[i][j]
-            if piece != None and piece.is_highlighted: #if piece is highlighted, highlight the square
+            if piece != None and piece.is_highlighted:  # if piece is highlighted, highlight the square
                 pygame.draw.rect(window, HIGHLIGHT_COLOR, square)
             else:
                 pygame.draw.rect(window, color, square)
 
 
-def draw_player_pieces(player): #draw a player's pieces
+def draw_captured_pieces():
+    for i in range(len(white_player.dead_pieces)):
+        draw_x, draw_y = (i * SQUARE_SIZE / 2) + SQUARE_SIZE * .25, Y_OFFSET - SQUARE_SIZE * 1.25
+        if  i >= BOARD_WIDTH: # draw pieces on the second row if there are too many
+            draw_y -= SQUARE_SIZE/2
+            draw_x -= (BOARD_WIDTH * SQUARE_SIZE / 2)
+        window.blit(get_image(white_player.dead_pieces[i]), (draw_x, draw_y))
+    for i in range(len(black_player.dead_pieces)):
+        draw_x, draw_y = (i * SQUARE_SIZE / 2) + SQUARE_SIZE * .25, Y_OFFSET + (BOARD_HEIGHT * SQUARE_SIZE) + SQUARE_SIZE * 0.25
+        if  i >= BOARD_WIDTH: # draw pieces on the second row if there are too many
+            draw_y += SQUARE_SIZE/2
+            draw_x -= (BOARD_WIDTH * SQUARE_SIZE / 2)
+        window.blit(get_image(black_player.dead_pieces[i]), (draw_x, draw_y))
+
+
+def draw_player_pieces(player):  # draw a player's pieces
     for piece in player.pieces:
-        if piece.spriteLoc in sprite_cache: # load sprite from cache
-            piece_image = sprite_cache[piece.spriteLoc]
-        else:
-            piece_sprite = pygame.image.load(piece.spriteLoc) #load sprite
-            sprite_scalar = SQUARE_SIZE / max(piece_sprite.get_width(), piece_sprite.get_height()) #get scalar to maintain aspect ratio
-            piece_image = pygame.transform.smoothscale(piece_sprite, (piece_sprite.get_width() * sprite_scalar, piece_sprite.get_height() * sprite_scalar))
-            sprite_cache[piece.spriteLoc] = piece_image # add created sprite to cache
+        piece_image = get_image(piece)
         window.blit(piece_image, (piece.loc_x * SQUARE_SIZE, piece.loc_y * SQUARE_SIZE + Y_OFFSET))
 
-    highlighted_piece = get_highlighted_piece() # draw target squares for highlighted piece
+    highlighted_piece = get_highlighted_piece()  # draw target squares for highlighted piece
     if highlighted_piece is not None:
         for square in highlighted_piece.available_squares:
-            pygame.draw.circle(window, HIGHLIGHT_COLOR, (square[0] * SQUARE_SIZE + SQUARE_SIZE / 2, square[1] * SQUARE_SIZE + SQUARE_SIZE / 2 + Y_OFFSET), SQUARE_SIZE / 6)
+            pygame.draw.circle(window, HIGHLIGHT_COLOR, (
+            square[0] * SQUARE_SIZE + SQUARE_SIZE / 2, square[1] * SQUARE_SIZE + SQUARE_SIZE / 2 + Y_OFFSET),
+                               SQUARE_SIZE / 6)
+
+
+def get_image(piece):
+    if piece.spriteLoc in image_cache:  # load sprite from cache
+        piece_image = image_cache[piece.spriteLoc]
+    else:
+        piece_sprite = pygame.image.load(piece.spriteLoc)  # load sprite
+        sprite_scalar = SQUARE_SIZE / max(piece_sprite.get_width(),
+                                          piece_sprite.get_height())  # get scalar to maintain aspect ratio
+        piece_image = pygame.transform.smoothscale(piece_sprite, (
+            piece_sprite.get_width() * sprite_scalar, piece_sprite.get_height() * sprite_scalar))
+        image_cache[piece.spriteLoc] = piece_image  # add created sprite to cache
+    return piece_image
 
 
 def calculate_squares_all_pieces():
@@ -136,18 +161,22 @@ def calculate_squares_all_pieces():
     for piece in black_player.pieces:
         piece.check_squares(board)
 
-def draw(): #combines all draw functions
+
+def draw():  # combines all draw functions
     window.fill((128, 128, 128))
     draw_board()
     draw_player_pieces(white_player)
     draw_player_pieces(black_player)
+    draw_captured_pieces()
     pygame.display.update()
+
 
 def clear_highlights():
     for piece in white_player.pieces:
         piece.remove_highlight()
     for piece in black_player.pieces:
         piece.remove_highlight()
+
 
 def get_highlighted_piece():
     for piece in white_player.pieces:
@@ -158,6 +187,7 @@ def get_highlighted_piece():
             return piece
     return None
 
+
 def switch_player_turn():
     global player_turn
     if player_turn == PieceColor.WHITE:
@@ -165,33 +195,34 @@ def switch_player_turn():
     else:
         player_turn = PieceColor.WHITE
 
-def handle_click(click_position): # is run every time a click event is triggered
-    square_x = math.floor(click_position[0] / SQUARE_SIZE) # convert event coordinates to board squares
+
+def handle_click(click_position):  # is run every time a click event is triggered
+    square_x = math.floor(click_position[0] / SQUARE_SIZE)  # convert event coordinates to board squares
     square_y = math.floor((click_position[1] - Y_OFFSET) / SQUARE_SIZE)
-    if Y_OFFSET < click_position[1] < (Y_OFFSET + BOARD_HEIGHT * SQUARE_SIZE): # if the click was on the board
+    if Y_OFFSET < click_position[1] < (Y_OFFSET + BOARD_HEIGHT * SQUARE_SIZE):  # if the click was on the board
         clicked_piece = board[square_y][square_x]  # get the piece at the clicked location
-        if clicked_piece is not None and clicked_piece.color is player_turn: # clicked on playing color's piece
+        if clicked_piece is not None and clicked_piece.color is player_turn:  # clicked on playing color's piece
             clear_highlights()
             clicked_piece.add_highlight()
         else:
             highlighted_piece = get_highlighted_piece()
-            if highlighted_piece is not None: # a piece has been highlighted
+            if highlighted_piece is not None:  # a piece has been highlighted
                 for square in highlighted_piece.available_squares:
-                    if square == [square_x, square_y]: # clicked square is a target
-                        if isinstance(highlighted_piece, Pawn): # keep track of pawn double moves
+                    if square == [square_x, square_y]:  # clicked square is a target
+                        if isinstance(highlighted_piece, Pawn):  # keep track of pawn double moves
                             highlighted_piece.move_count += 1
                         board[highlighted_piece.loc_y][highlighted_piece.loc_x] = None
 
                         # capture piece
                         if board[square_y][square_x] is not None:
-                            if(player_turn == PieceColor.WHITE):
+                            if (player_turn == PieceColor.WHITE):
                                 black_player.remove_piece(board[square_y][square_x])
                             else:
                                 white_player.remove_piece(board[square_y][square_x])
                         board[square_y][square_x] = None
 
                         board[square_y][square_x] = highlighted_piece
-                        highlighted_piece.set_loc(square_x, square_y) # move piece to clicked square
+                        highlighted_piece.set_loc(square_x, square_y)  # move piece to clicked square
                         calculate_squares_all_pieces()
                         switch_player_turn()
             clear_highlights()
